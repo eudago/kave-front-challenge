a52ad16d14b062f41e12e98112267927a1de83ab
Eudald Dachs <eudago@gmail.com>
2022-08-07 20:50:04 +0200

    change favicon and add subheader gradient

--- components/SubHeader.tsx
+++ components/SubHeader.tsx
@@ -3,7 +3,7 @@ import { Flex } from "@components/Flex";
 
 const SubHeaderStyled = styled('div', {
     width: '100%',
-    backgroundImage: 'url(./images/home-image.png)',
+    backgroundImage: 'linear-gradient(180deg, rgba(0, 0, 0, 0.0001) 0%, rgba(0, 0, 0, 0.487449) 90.3%), url(./images/home-image.png)',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundAttachment: 'fixed',
@@ -21,6 +21,7 @@ interface SubHeaderProps {
 }
 
 export default function SubHeader({ children }: SubHeaderProps) {
+
   return (
     <SubHeaderStyled>
         {!!children && <Flex direction="column" justify="end" css={{
